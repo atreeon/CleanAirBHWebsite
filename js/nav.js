fa4493ad1318d3a90f4abdfbd69729baf4dfe068
Atree <atreeonhill@gmail.com>
2021-12-24 18:45:36 +0000

    Added flyers page

--- js/nav.js
+++ js/nav.js
@@ -6,3 +6,14 @@ fetch('nav.html')
         newelem.innerHTML = text;
         oldelem.parentNode.replaceChild(newelem, oldelem);
     })
+    
+// Script to open and close sidebar
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
